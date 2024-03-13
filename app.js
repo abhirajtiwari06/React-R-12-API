98fbd63519c85a61ba539a55eda25b7d260dc7bc
abhirajtiwari06 <abhiraj080904@gmail.com>
2024-03-13 22:26:32 +0530

    update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const app = express();
-console>log("hello")
+// console>log("hello")
 
-
+//dbconnection 
+require("./models/dbconfig").dbconnection()
 
 app.listen(process.env.PORT, () => {
     console.log(`The Server is running on port ${process.env.PORT}`);
